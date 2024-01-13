daedeff4f52d8ded3b5112c48324655212a438b4
Emir Cica <emircica.ec@gmail.com>
2024-01-14 02:41:19 +0300

    Now it waits between the welcoming message and the generated password!

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import random
 import string
-
+import time
 print("Welcome to the password generator which gives you strong but ridiculous passwords!")
 
-def generate_password(length=12):
+
+time.sleep(5)
+
+print("Password is coming!")
+
+time.sleep(5)
+
+def generate_password(length =12):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for i in range(length))
+
     return password
-
-
 my_password = generate_password()
+
 print("Generated Password:" , my_password)
